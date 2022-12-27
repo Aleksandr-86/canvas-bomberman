79db98d4aa027d3940c2ea24e2dd7402357ed0b2
German <bz5h7@yandex.ru>
2022-12-27 19:17:58 +0300

    build: react eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
