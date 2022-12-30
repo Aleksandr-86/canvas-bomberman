79a48b27de52038be1217879f9567e8ee52e4d46
Aleksandr-86 <Nomas86@yandex.ru>
2022-12-30 17:36:52 +0300

    refactor(client): rebuild landing page

--- packages/client/src/pages/landing/index.tsx
+++ packages/client/src/pages/landing/index.tsx
@@ -1,16 +1,83 @@
-const Landing = () => {
+import { Link } from 'react-router-dom'
+
+import styles from './landing.module.css'
+
+export const Landing = () => {
+  const isAuth = false
+  const displayName = 'Алекс'
+
+  let content: JSX.Element
+
+  if (isAuth) {
+    content = (
+      <>
+        <div className={styles.landing__description_top}>
+          Привет {displayName}! <br />
+          Спешим напомнить, что ты в любой момент можешь освежить свои знания по
+          игре, перечитав{' '}
+          <Link className={styles.landing__link} to="/rules">
+            правила
+          </Link>{' '}
+          и посмотреть{' '}
+          <Link className={styles.landing__link} to="/leaderboard">
+            таблицу лидеров!
+          </Link>
+        </div>
+
+        <Link to="/game">
+          <button className={styles.landing__button}>Погнали</button>
+        </Link>
+      </>
+    )
+  } else {
+    content = (
+      <>
+        <div className={styles.landing__description_top}>
+          Привет, мы рады приветствовать тебя! <br />
+          Прежде чем ты начнёшь взрывать всё вокруг, загляни в{' '}
+          <Link className={styles.landing__link} to="/rules">
+            правила
+          </Link>
+          . Уверены, это поможет тебе возглавить нашу{' '}
+          <Link className={styles.landing__link} to="/leaderboard">
+            таблицу лидеров!
+          </Link>
+        </div>
+
+        <div className={styles['landing__button-wrapper']}>
+          <Link to="/sign-in">
+            <button className={styles.landing__button}>Войти</button>
+          </Link>
+
+          <Link to="/sign-up">
+            <button className={styles.landing__button}>
+              Зарегистрироваться
+            </button>
+          </Link>
+        </div>
+      </>
+    )
+  }
+
   return (
-    <>
-      <h2>Welcome to bomberman</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni magnam
-        voluptate ex est, voluptatem debitis laudantium sequi omnis aliquid
-        officia eum sed obcaecati pariatur eveniet perspiciatis mollitia
-        perferendis ratione consequuntur quibusdam eos vero architecto nam quam
-        aliquam. Ex, facilis at.
-      </p>
-    </>
+    <div className={styles.landing}>
+      <div className={styles.landing__wrapper}>
+        <img
+          className={styles.landing__logo}
+          src="src/assets/images/bomberman-logo.png"
+          alt="bomberman-log"
+        />
+        {content}
+        <div className={styles.landing__description_bottom}>
+          Есть вопросы? Скорее переходи к нам на{' '}
+          <Link className={styles.landing__link} to="/forum">
+            форум
+          </Link>
+        </div>
+      </div>
+      <div className={styles['landing__img-wrapper']}>
+        <img className="landing__hero-img" src="src/assets/images/hero.png" />
+      </div>
+    </div>
   )
 }
-
-export { Landing }
