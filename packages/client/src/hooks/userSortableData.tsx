313f8524e3ad2c8ac41f5ce897cea360fd4d5e63
Aleksandr-86 <Nomas86@yandex.ru>
2023-01-02 17:53:19 +0300

    feat(client): add a hook for sorting data

---
+++ packages/client/src/hooks/userSortableData.tsx
@@ -0,0 +1,50 @@
+import { useMemo, useState } from 'react'
+
+interface Items {
+  [index: string]: string | number
+}
+
+interface SortConfig {
+  key: string | number
+  direction: string
+}
+
+export const useSortableData = (items: Items[]) => {
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null)
+
+  const sortedItems = useMemo(() => {
+    const sortableItems = [...items]
+
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1
+        }
+
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1
+        }
+
+        return 0
+      })
+    }
+
+    return sortableItems
+  }, [items, sortConfig])
+
+  const requestSort = (key: string) => {
+    let direction = 'ascending'
+
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'ascending'
+    ) {
+      direction = 'descending'
+    }
+
+    setSortConfig({ key, direction })
+  }
+
+  return { items: sortedItems, requestSort, sortConfig }
+}
