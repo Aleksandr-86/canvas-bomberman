9b4093937c5dcc86f9d49b256c9f1674d5774c51
Aleksandr-86 <Nomas86@yandex.ru>
2023-01-08 14:55:08 +0300

    refactor(client): change text sample in App.test.tsx

--- packages/client/src/app/App.test.tsx
+++ packages/client/src/app/App.test.tsx
@@ -1,7 +1,7 @@
 import { App } from '.'
 import { render, screen } from '@testing-library/react'
 
-const appContent = 'Lorem ipsum'
+const appContent = 'Есть вопросы?'
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
