259ec2f4308c11458604616d9677da8a9d556940
Aleksandr-86 <Nomas86@yandex.ru>
2023-01-08 20:02:45 +0300

    style(client): fix some stylelint issues

--- packages/client/src/pages/landing/landing.tsx
+++ packages/client/src/pages/landing/landing.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Button } from '../../components/button/button'
-
 import baseStyles from '../../app/app.module.css'
 import styles from './landing.module.css'
 
