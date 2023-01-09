aa06345434b40916f5ab80c61ae08f0b9c87d654
archebaldo <zakharov90@gmail.com>
2023-01-09 18:27:46 +0300

    feat(client): add ErrorBoundary to project

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
+import { ErrorBoundary } from './components/error-boundary/error-boundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 )
