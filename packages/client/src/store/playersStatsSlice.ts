a9d602ec009878f85c0a67d6887a74e11437292b
Aleksandr-86 <Nomas86@yandex.ru>
2023-01-10 04:19:19 +0300

    feat(client): add playersStatsSlice.tsx to the store folder

---
+++ packages/client/src/store/playersStatsSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+declare interface PlayerStats {
+  id: number
+  place: number
+  name: string
+  score: number
+  games: number
+  totalTime: number
+}
+
+type PlayersStatsState = Array<PlayerStats>
+
+const initialState = [
+  {
+    id: 1,
+    place: 1,
+    name: 'Bionic',
+    score: 27500,
+    games: 10,
+    totalTime: 61,
+  },
+  {
+    id: 2,
+    place: 2,
+    name: 'Chrome Road',
+    score: 15150,
+    games: 5,
+    totalTime: 36,
+  },
+  {
+    id: 3,
+    place: 3,
+    name: 'Bladewatch',
+    score: 13500,
+    games: 4,
+    totalTime: 30,
+  },
+  {
+    id: 4,
+    place: 4,
+    name: 'Marshmallow',
+    score: 12100,
+    games: 3,
+    totalTime: 23,
+  },
+  {
+    id: 5,
+    place: 5,
+    name: 'Arctic Realm',
+    score: 9800,
+    games: 3,
+    totalTime: 19,
+  },
+] as PlayersStatsState
+
+const playersStatsSlice = createSlice({
+  name: 'playersStats',
+  initialState,
+  reducers: {},
+})
+
+export default playersStatsSlice.reducer
