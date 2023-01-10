fe720c8df654c3eb560929cb066e4fa10097aa40
Aleksandr-86 <Nomas86@yandex.ru>
2023-01-10 04:43:27 +0300

    refactor(client): exclude local and incorporate global state on leaderboard page

--- packages/client/src/pages/leaderboard/index.tsx
+++ packages/client/src/pages/leaderboard/index.tsx
@@ -1,87 +1,31 @@
-import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { RootState } from '../../store'
+import { NavigationBar } from '../../components/navigation-bar/navigation-bar'
 import { Table } from '../../components/table/table'
 
 import baseStyles from '../../app/app.module.css'
 import styles from './leaderboard.module.css'
 
-// Заглушка
-const tableData = [
-  {
-    id: 1,
-    place: 1,
-    name: 'Bionic',
-    score: 27500,
-    games: 10,
-    totalTime: 61,
-  },
-  {
-    id: 2,
-    place: 2,
-    name: 'Chrome Road',
-    score: 15150,
-    games: 5,
-    totalTime: 36,
-  },
-  {
-    id: 3,
-    place: 3,
-    name: 'Bladewatch',
-    score: 13500,
-    games: 4,
-    totalTime: 30,
-  },
-  {
-    id: 4,
-    place: 4,
-    name: 'Marshmallow',
-    score: 12100,
-    games: 3,
-    totalTime: 23,
-  },
-  {
-    id: 5,
-    place: 5,
-    name: 'Arctic Realm',
-    score: 9800,
-    games: 3,
-    totalTime: 19,
-  },
-]
-
 export const Leaderboard = () => {
-  const displayName = 'Алекс'
-  const [isAuth, setIsAuth] = useState(true)
-
-  // Навигационная панель
-  const NavBarLogged = () => (
-    <div className={styles['leaderboard__nav-bar']}>
-      <img
-        src="src/assets/images/avatar.png"
-        alt="user-avatar"
-        draggable="false"
-      />
-      <Link className={styles.leaderboard__link} to="/profile">
-        {displayName}
-      </Link>
-      <Link
-        className={styles.leaderboard__link}
-        to="/"
-        onClick={() => setIsAuth(false)}>
-        Выйти
-      </Link>
-    </div>
+  const userState = useSelector((state: RootState) => state.user)
+  const playersStatsState = useSelector(
+    (state: RootState) => state.playersStats
   )
 
-  const navBar = isAuth ? <NavBarLogged /> : null
+  const navBar = userState.isAuth ? <NavigationBar /> : null
 
   return (
     <div className={styles.leaderboard}>
       {navBar}
 
       <div className={styles.leaderboard__wrapper}>
-        <Table caption="Таблица лидеров" userStats={tableData} />
+        <div className={styles['leaderboard__table-caption']}>
+          Таблица лидеров
+        </div>
+
+        <Table playersStats={playersStatsState} />
 
         <Link className={baseStyles.link_button} to="/">
           На главную
