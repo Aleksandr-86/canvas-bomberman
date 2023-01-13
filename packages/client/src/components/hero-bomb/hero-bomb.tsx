5e59c392436278318c0fb93a989279f11f191ae0
archebaldo <zakharov90@gmail.com>
2023-01-13 12:10:31 +0300

    refactor(client): delete unused styles

--- packages/client/src/components/hero-bomb/hero-bomb.tsx
+++ packages/client/src/components/hero-bomb/hero-bomb.tsx
@@ -1,11 +1,8 @@
-import styles from './hero-bomb.module.css'
-
 import heroBomb from '../../assets/images/hero-bomb.png'
 
 export const HeroBomb = () => {
   return (
     <img
-      className={styles.heroBomb}
       src={heroBomb}
       width={426}
       height={426}
