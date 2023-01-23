c4d6c1736b87f753ad4ef85fd8df797b6308ae40
archebaldo <zakharov90@gmail.com>
2023-01-23 22:21:24 +0300

    fix(client): improve code that fullscreen mode work in any keyboard layout and add types for keys

--- packages/client/src/env.d.ts
+++ packages/client/src/env.d.ts
@@ -1,1 +1,28 @@
 /// <reference types="vite/client" />
+
+export type KeyboardKeys =
+  | `Q`
+  | `W`
+  | `E`
+  | `T`
+  | `Y`
+  | `U`
+  | `I`
+  | `O`
+  | `P`
+  | `A`
+  | `S`
+  | `D`
+  | `F`
+  | `G`
+  | `H`
+  | `J`
+  | `K`
+  | `L`
+  | `Z`
+  | `X`
+  | `C`
+  | `V`
+  | `B`
+  | `N`
+  | `M`
--- packages/client/src/hooks/useFullScreen.tsx
+++ packages/client/src/hooks/useFullScreen.tsx
@@ -1,16 +1,16 @@
 import { type MutableRefObject, useCallback, useEffect } from 'react'
 import { toggleFullScreen } from '../features/toggleFullScreen/toggleFullScreen'
+import type { KeyboardKeys } from '../env'
 
 export const useFullScreen = <T extends HTMLElement | null>(
   ref: MutableRefObject<T>,
-  activeKey = `f`
+  activeKey: KeyboardKeys = `F`
 ) => {
   const onKeyDown = useCallback(
     (evt: KeyboardEvent) => {
-      const activeKeyLowerCase = activeKey.toLowerCase()
-      const key = evt.key.toLowerCase()
+      const key = evt.code
 
-      if (key === activeKeyLowerCase) {
+      if (key === `Key${activeKey}`) {
         if (ref.current) {
           toggleFullScreen(ref.current)
         }
