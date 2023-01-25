30b02d5b0d6c784f58052b9c805d796bb48a73f9
German <bz5h7@yandex.ru>
2023-01-25 18:14:10 +0300

    refactor(client): clean up game scene

--- packages/client/src/features/game/bombermanScene.ts
+++ packages/client/src/features/game/bombermanScene.ts
@@ -1,61 +1,72 @@
-import { CELL_WIDTH, INITIAL_FIELD_STATE } from './const'
-import { Rect, Sprite } from './lib'
+import { CELL_WIDTH } from './const'
+import { Sprite } from './lib/gameObjects'
 import { type SceneContext, type Scene } from './lib/scene'
 import { Textures } from './lib/textures'
-import { type KindToTile, tilemap } from './lib/tilemap'
-import { Kind } from './types'
-
-const PLAYER_VELOCITY = CELL_WIDTH
-
-const tiles: KindToTile = {
-  [Kind.Empty]: (x, y) =>
-    new Sprite(
-      Textures.Green,
-      x * CELL_WIDTH,
-      y * CELL_WIDTH,
-      CELL_WIDTH,
-      CELL_WIDTH
-    ),
-  [Kind.Player]: (x, y) =>
-    new Sprite(
-      Textures.White,
-      x * CELL_WIDTH,
-      y * CELL_WIDTH,
-      CELL_WIDTH,
-      CELL_WIDTH
-    ),
-  [Kind.WallHard]: (x, y) =>
-    new Sprite(
-      Textures.Blue,
-      x * CELL_WIDTH,
-      y * CELL_WIDTH,
-      CELL_WIDTH,
-      CELL_WIDTH
-    ),
-}
+import { store } from '../../store'
+import { playerReset } from '../../store/gameSlice'
+import { Vec2 } from './utils/vec2'
+import { AnimatedSprite } from './lib/gameObjects/animatedSprite'
+import { placeBomb } from './gameActions'
+import { createTiles } from './createTiles'
+import { move, getField } from './gameActions'
 
 export const bombermanScene = {
   create: (scene: Scene) => {
+    const { x: doorx, y: doory } = store.getState().game.doorPosition
+
+    const door = new Sprite(
+      Textures.Blue,
+      CELL_WIDTH * doorx,
+      CELL_WIDTH * doory,
+      CELL_WIDTH,
+      CELL_WIDTH,
+      2
+    )
+
     const player = new Sprite(
-      Textures.White,
+      Textures.Blue,
       CELL_WIDTH,
       CELL_WIDTH,
       CELL_WIDTH,
       CELL_WIDTH,
+      2,
       'player'
     )
-    const map = tilemap(INITIAL_FIELD_STATE, tiles)
 
-    map.forEach(o => scene.add(o))
+    window.addEventListener('keypress', e => {
+      if (e.code === 'KeyR') {
+        store.dispatch(playerReset())
+      }
+    })
 
-    scene.add(player)
+    store.subscribe(() => {
+      const { doorPosition, playerPosition } = store.getState().game
+
+      if (Vec2.equals(doorPosition, playerPosition)) {
+        console.log('you won')
+      }
+      const { x, y } = Vec2.mul(playerPosition, CELL_WIDTH)
+
+      scene.clear()
+      const map = createTiles(getField())
+      map.forEach(o => scene.add(o))
+      scene.add(door)
+      scene.sort()
+      scene.add(player)
+      console.log(player)
+
+      player.x = x
+      player.y = y
+      player.width = CELL_WIDTH
+      player.height = CELL_WIDTH
+    })
   },
-  update: ({ kbd, scene, delta }: SceneContext) => {
-    const player = scene.getById('player')
+  update: ({ kbd }: SceneContext) => {
+    if (kbd.left) move(Vec2.Left)
+    if (kbd.right) move(Vec2.Right)
+    if (kbd.up) move(Vec2.Up)
+    if (kbd.down) move(Vec2.Down)
 
-    if (kbd.left) player.x -= PLAYER_VELOCITY * delta * 4
-    if (kbd.right) player.x += PLAYER_VELOCITY * delta * 4
-    if (kbd.up) player.y -= PLAYER_VELOCITY * delta * 4
-    if (kbd.down) player.y += PLAYER_VELOCITY * delta * 4
+    if (kbd.space) placeBomb()
   },
 }
