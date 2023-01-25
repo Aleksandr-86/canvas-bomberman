60d57f7e0145f7913ba871b44daa496c84a098b9
German <bz5h7@yandex.ru>
2023-01-25 17:48:58 +0300

    feat(client): field setup, bomb and player movement reducers

--- packages/client/src/store/gameSlice.ts
+++ packages/client/src/store/gameSlice.ts
@@ -1,7 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { PLAYER_STARTING_POSITION, EMPTY_FIELD } from '../features/game/const'
-import { Kind } from '../features/game/types'
-import { Vec2 } from '../features/game/utils'
+import {
+  PLAYER_STARTING_POSITION,
+  EMPTY_FIELD,
+  GRID_HEIGHT,
+  GRID_WIDTH,
+} from '../features/game/const'
+import { Kind, type FieldState } from '../features/game/types'
+import { delay, Vec2 } from '../features/game/utils'
+import { fillSoftWalls } from '../features/game/fillSoftWalls'
+import { explodeAdjacentSoftWalls } from '../features/game/explodeSoftWalls'
+import { AppDispatch } from './index'
 
 export enum GameStatus {
   START = 'START',
@@ -10,23 +18,32 @@ export enum GameStatus {
 }
 
 type GameState = {
+  field: FieldState
   status: GameStatus
   currentScore: number
   playerPosition: Vec2
   doorPosition: Vec2
+  bombs: Vec2[]
+  time: number
 }
 
 const INITIAL_STATE: GameState = {
   status: GameStatus.START,
   currentScore: 0,
-  doorPosition: new Vec2(7, 6),
+  doorPosition: new Vec2(GRID_WIDTH - 2, GRID_HEIGHT - 2),
   playerPosition: PLAYER_STARTING_POSITION,
+  bombs: [],
+  field: EMPTY_FIELD,
+  time: 300,
 }
 
 const gameSlice = createSlice({
   name: 'game',
   initialState: () => {
-    return INITIAL_STATE
+    return {
+      ...INITIAL_STATE,
+      field: fillSoftWalls(EMPTY_FIELD),
+    }
   },
   reducers: {
     setStatus: (state, action: PayloadAction<GameStatus>) => {
@@ -35,15 +52,57 @@ const gameSlice = createSlice({
     setCurrentScore: (state, action: PayloadAction<number>) => {
       state.currentScore = action.payload
     },
-    playerMoved: (state, action: PayloadAction<Vec2>) => {
-      const newPosition = Vec2.add(state.playerPosition, action.payload)
-
-      if (EMPTY_FIELD[newPosition.y][newPosition.x] === Kind.Empty) {
-        state.playerPosition = newPosition
-      }
+    playerMoved: (state, { payload }: PayloadAction<Vec2>) => {
+      state.playerPosition = payload
+    },
+    playerReset: state => {
+      state.playerPosition = PLAYER_STARTING_POSITION
+    },
+    bombPlaced: (state, { payload: { x, y } }: PayloadAction<Vec2>) => {
+      state.field[y][x] = Kind.Bomb
+    },
+    scoreIncreased: (state, { payload }: PayloadAction<number>) => {
+      state.currentScore += payload
+    },
+    bombExploded: ({ field }, { payload }: PayloadAction<Vec2>) => {
+      explodeAdjacentSoftWalls(field, payload)
+    },
+    timerUpdated: (state, { payload }: PayloadAction<number>) => {
+      state.time -= payload * 0.001 /* payload is in ms */
     },
   },
 })
 
+export const bombSet = (pos: Vec2) => async (dispatch: AppDispatch) => {
+  dispatch(bombPlaced(pos))
+  await delay(800)
+  dispatch(bombExploded(pos))
+}
+
+let previousTime = performance.now()
+let gameIntervalId = 0
+
+export const gameStarted = () => (dispatch: AppDispatch) => {
+  gameIntervalId = window.setInterval(() => {
+    const now = performance.now()
+    const dt = now - previousTime
+    previousTime = now
+    dispatch(timerUpdated(dt))
+  }, 1000)
+}
+
+export const gameEnded = () => () => {
+  clearInterval(gameIntervalId)
+}
+
 export const gameReducer = gameSlice.reducer
-export const { setStatus, setCurrentScore, playerMoved } = gameSlice.actions
+export const {
+  setStatus,
+  setCurrentScore,
+  playerMoved,
+  playerReset,
+  bombPlaced,
+  bombExploded,
+  scoreIncreased,
+  timerUpdated,
+} = gameSlice.actions
--- packages/client/src/store/selectors.ts
+++ packages/client/src/store/selectors.ts
@@ -4,3 +4,8 @@ export const getUser = (state: RootState) => state.user
 export const getPlayersStats = (state: RootState) => state.playersStats
 export const getCurrentScore = (state: RootState) => state.game.currentScore
 export const getGameStatus = (state: RootState) => state.game.status
+export const selectPlayerPosition = (state: RootState) =>
+  state.game.playerPosition
+export const selectField = (state: RootState) => state.game.field
+export const selectBombs = (state: RootState) => state.game.bombs
+export const selectTime = (state: RootState) => state.game.time
