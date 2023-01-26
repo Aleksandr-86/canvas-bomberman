87c3f2fb6284e6e4b234e691fd99ec801103e3d5
archebaldo <zakharov90@gmail.com>
2023-01-26 14:04:33 +0300

    refactor(client): update snapshots after merge main

--- packages/client/src/pages/signIn/signIn.snap.test.tsx
+++ packages/client/src/pages/signIn/signIn.snap.test.tsx
@@ -1,9 +1,20 @@
 import * as renderer from 'react-test-renderer'
 import { SignIn } from './signIn'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+
+const mockStore = configureStore([])
+const store = mockStore({})
 
 describe(`Snapshot test page SignIn`, () => {
-  test(`Should correct render (without props)`, () => {
-    const tree = renderer.create(<SignIn />).toJSON()
+  test(`Should correct render (with store)`, () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <SignIn />
+        </Provider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
