0a09863b621e4736fbe69f2e205d576cf23dce91
archebaldo <zakharov90@gmail.com>
2023-01-30 12:21:39 +0300

    refactor(client): update snapshot test for Profile page

--- packages/client/src/pages/profile/profile.snap.test.tsx
+++ packages/client/src/pages/profile/profile.snap.test.tsx
@@ -1,9 +1,32 @@
 import * as renderer from 'react-test-renderer'
 import { Profile } from './profile'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router'
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore([])
+const store = mockStore({
+  user: {
+    displayName: `mockDisplayName`,
+    login: `mockLogin`,
+    firstName: `mockFirstName`,
+    secondName: `mockSecondName`,
+    email: `mockEmail`,
+    phone: 8900000000,
+  },
+})
 
 describe(`<Profile/> snapshot test cases`, () => {
   test(`<Profile/> should render <div className=profile>`, () => {
-    const tree = renderer.create(<Profile />).toJSON()
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Profile />
+          </MemoryRouter>
+        </Provider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
