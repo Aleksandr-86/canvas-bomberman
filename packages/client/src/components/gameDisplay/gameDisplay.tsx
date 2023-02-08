2da4b08136d0f26bb5566c94906a1d31318973d8
German <bz5h7@yandex.ru>
2023-02-08 18:41:17 +0300

    refactor(client): update GameDisplay, add GameScore

--- packages/client/src/components/gameDisplay/gameDisplay.tsx
+++ packages/client/src/components/gameDisplay/gameDisplay.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react'
 import { bombermanScene } from '../../features/game/bombermanScene'
 import { Game } from '../../features/game/lib'
 import { GameOverlay } from '../gameOverlay/gameOverlay'
+import { CAMERA_HEIGHT, CAMERA_WIDTH } from '../../features/game/const'
 import styles from './gameDisplay.module.css'
 
 export const GameDisplay: React.FC = () => {
@@ -14,20 +15,21 @@ export const GameDisplay: React.FC = () => {
     if (!canvas) return
 
     const game = new Game({
-      height: 720,
-      width: 1280,
-      backgroundColor: 'teal',
+      height: CAMERA_HEIGHT,
+      width: CAMERA_WIDTH,
+      backgroundColor: '#64b0ff',
       root: canvas,
       scene: bombermanScene,
     })
 
     game.start()
+    return () => game.stop()
   }, [])
 
   return (
     <div className={styles.gameDisplay}>
       <GameScore />
-      <canvas ref={gameRef} width={1280} height={720} />
+      <canvas ref={gameRef} width={CAMERA_WIDTH} height={CAMERA_HEIGHT} />
       <GameOverlay onReloadGame={() => undefined} />
     </div>
   )
