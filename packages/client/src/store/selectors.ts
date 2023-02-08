0a28faf01a41d1eca59081362c5aaf90f752dcca
German <bz5h7@yandex.ru>
2023-02-08 18:26:42 +0300

    feat(client): game stats selectors

--- packages/client/src/store/selectors.ts
+++ packages/client/src/store/selectors.ts
@@ -1,11 +1,35 @@
+import { createSelector } from '@reduxjs/toolkit'
 import { RootState } from './index'
 
 export const getUser = (state: RootState) => state.user
 export const getPlayersStats = (state: RootState) => state.playersStats
-export const getCurrentScore = (state: RootState) => state.game.currentScore
 export const getGameStatus = (state: RootState) => state.game.status
+export const getCurrentScore = (state: RootState) => state.game.currentScore
 export const selectPlayerPosition = (state: RootState) =>
   state.game.playerPosition
-export const selectField = (state: RootState) => state.game.field
 export const selectBombs = (state: RootState) => state.game.bombs
-export const selectTime = (state: RootState) => state.game.time
+export const selectSoftWalls = (state: RootState) => state.game.softWalls
+export const selectBuffs = (state: RootState) => state.game.buffs
+export const selectDoor = (state: RootState) => state.game.doorPosition
+export const selectGameObjects = createSelector(
+  [selectBombs, selectSoftWalls, selectBuffs],
+  (bombs, walls, buffs) => {
+    return {
+      bombs,
+      walls,
+      buffs,
+    }
+  }
+)
+
+export const selectActiveBuffList = (store: RootState) =>
+  Object.entries(store.game.activeBuffs)
+    .filter(([_, active]) => active)
+    .map(([buff]) => buff)
+
+export const selectGameStats = (store: RootState) => {
+  return {
+    score: store.game.currentScore,
+    timeLeft: Math.trunc(store.game.time),
+  }
+}
