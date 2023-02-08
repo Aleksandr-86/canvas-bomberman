8f9ef32862c928d930668f7f8bc6a9997348a548
German <bz5h7@yandex.ru>
2023-02-08 18:39:56 +0300

    feat(client): add GameScore component

--- packages/client/src/components/gameScore/gameScore.tsx
+++ packages/client/src/components/gameScore/gameScore.tsx
@@ -1,13 +1,33 @@
-import { getCurrentScore, selectTime } from '../../store/selectors'
 import { useAppSelector } from '../../store/hooks'
+import gameScore from './gameScore.module.css'
+import bombermanAtlas from '../../assets/images/nesBomberman3x.json'
+import { selectActiveBuffList, selectGameStats } from '../../store/selectors'
 
 export const GameScore = () => {
-  const score = useAppSelector(getCurrentScore)
-  const time = useAppSelector(selectTime)
+  const { score, timeLeft } = useAppSelector(selectGameStats)
+  const activeBuffList = useAppSelector(selectActiveBuffList)
 
   return (
-    <p>
-      Score: {score} | Time left: {Math.trunc(time)}
-    </p>
+    <div className={gameScore.panel}>
+      <span>Time Left: {timeLeft}</span>
+      <span>Score: {score}</span>
+      {activeBuffList.map(buffName => {
+        const { x, y } =
+          bombermanAtlas.entries[
+            buffName as keyof typeof bombermanAtlas.entries
+          ]
+        const key = `${x}${y}`
+        const backgroundPosition = `${-x * 48}px ${-y * 48}px`
+
+        return (
+          <div
+            key={key}
+            className={gameScore.buffTile}
+            style={{
+              backgroundPosition,
+            }}></div>
+        )
+      })}
+    </div>
   )
 }
