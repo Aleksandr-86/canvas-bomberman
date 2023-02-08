420a6016ec3cae506b0c9ff8f60a551c45b36377
German <bz5h7@yandex.ru>
2023-02-08 17:42:01 +0300

    refactor(client): create gameObjects in own class

--- packages/client/src/features/game/lib/gameObjects/gameObjectFactory.ts
+++ packages/client/src/features/game/lib/gameObjects/gameObjectFactory.ts
@@ -14,35 +14,35 @@ type TileGridConfig<C extends number = number> = {
   depth: number
 }
 
-export class GameObjectFactory {
+export class GameObjectCreator {
   constructor(private scene: SceneContext) {}
 
-  private register(object: SceneObject | SceneObject[]) {
-    this.scene.displayList = Array.isArray(object)
-      ? this.scene.displayList.concat(object)
-      : [...this.scene.displayList, object]
-    this.scene.depthSort()
-  }
-
-  /**
-   * Create and add rectangle to scene
-   */
   rect(x: number, y: number, width: number, height: number, fill: string) {
     const gameObject = new Rect(x, y, width, height, fill)
 
-    this.register(gameObject)
     return gameObject
   }
 
-  /**
-   * Create and add grid of tiles to scene
-   *
-   * From 1D array (row-major order) of tile kinds and mapping of tile kinds to colon-separated string describing texture and its frame
-   *
-   * If texture in mapping not found, cell is skipped
-   *
-   * `<textureKey>:<textureFrameKey>`
-   */
+  sprite(
+    x: number,
+    y: number,
+    textureKey: string,
+    frameKey = '__base',
+    width?: number,
+    height?: number
+  ) {
+    const texture =
+      this.scene.textures.get(textureKey) ||
+      (this.scene.textures.get('white') as Texture)
+
+    const gameObject = new Sprite(x, y, texture, frameKey)
+
+    if (width) gameObject.width = width
+    if (height) gameObject.height = height
+
+    return gameObject
+  }
+
   tileGrid({ grid, gridWidth, cellSize, cells, depth }: TileGridConfig) {
     const tiles = []
 
@@ -70,6 +70,43 @@ export class GameObjectFactory {
       tiles.push(sprite)
     }
 
+    return tiles
+  }
+}
+
+export class GameObjectFactory {
+  public creator
+  constructor(private scene: SceneContext) {
+    this.creator = new GameObjectCreator(scene)
+  }
+
+  private register(object: SceneObject | SceneObject[]) {
+    this.scene.displayList = Array.isArray(object)
+      ? this.scene.displayList.concat(object)
+      : [...this.scene.displayList, object]
+    this.scene.depthSort()
+  }
+
+  /**
+   * Create and add rectangle to scene
+   */
+  rect(x: number, y: number, width: number, height: number, fill: string) {
+    const gameObject = this.creator.rect(x, y, width, height, fill)
+    this.register(gameObject)
+    return gameObject
+  }
+
+  /**
+   * Create and add grid of tiles to scene
+   *
+   * From 1D array (row-major order) of tile kinds and mapping of tile kinds to colon-separated string describing texture and its frame
+   *
+   * If texture in mapping not found, cell is skipped
+   *
+   * `<textureKey>:<textureFrameKey>`
+   */
+  tileGrid(config: TileGridConfig) {
+    const tiles = this.creator.tileGrid(config)
     this.register(tiles)
     return tiles
   }
@@ -77,12 +114,23 @@ export class GameObjectFactory {
   /**
    * Create and add Sprite with given key and frame to scene
    */
-  sprite(x: number, y: number, textureKey: string, frameKey = '__base') {
-    const texture =
-      this.scene.textures.get(textureKey) ||
-      (this.scene.textures.get('white') as Texture)
+  sprite(
+    x: number,
+    y: number,
+    textureKey: string,
+    frameKey = '__base',
+    width?: number,
+    height?: number
+  ) {
+    const gameObject = this.creator.sprite(
+      x,
+      y,
+      textureKey,
+      frameKey,
+      width,
+      height
+    )
 
-    const gameObject = new Sprite(x, y, texture, frameKey)
     this.register(gameObject)
     return gameObject
   }
