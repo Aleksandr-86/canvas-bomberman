cfb383f07b46acd36705b8cd84fb820d04ed1ab8
German <bz5h7@yandex.ru>
2023-02-21 13:03:46 +0300

    refactor(client): new Point class

--- packages/client/src/features/game/utils/point.ts
+++ packages/client/src/features/game/utils/point.ts
@@ -1,36 +1,78 @@
-export type TPoint = { x: number; y: number }
+export type PointLike = { x: number; y: number }
 
-export const Point = {
-  New: (x: number, y: number) => ({
-    x,
-    y,
-  }),
-  get Left() {
-    return { x: -1, y: 0 }
-  },
-  get Right() {
-    return { x: 1, y: 0 }
-  },
-  get Up() {
-    return { x: 0, y: -1 }
-  },
-  get Down() {
-    return { x: 0, y: 1 }
-  },
-  get Zero() {
-    return { x: 0, y: 0 }
-  },
+export class Point {
+  constructor(public x = 0, public y = 0) {}
 
-  mul: (s: number) => (p: TPoint) => ({ x: p.x * s, y: p.y * s }),
-  add: (p1: TPoint) => (p2: TPoint) => ({ x: p1.x + p2.x, y: p1.y + p2.y }),
-  equals: (p1: TPoint) => (p2: TPoint) => p1.x === p2.x && p1.y === p2.y,
-  copy: (p: TPoint) => ({ x: p.x, y: p.y }),
-  lerp: (from: TPoint) => (to: TPoint) => (t: number) => {
-    if (t > 1 || t < 0) throw new Error('t should be between 0 and 1')
+  dot(p: PointLike) {
+    return this.x * p.x + this.y + p.y
+  }
 
-    return {
-      x: from.x + (to.x - from.x) * t,
-      y: from.y + (to.y - from.y) * t,
+  mag() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2)
+  }
+
+  mul(p: PointLike) {
+    return new Point(this.x * p.x, this.y * p.y)
+  }
+
+  scale(by: number) {
+    return new Point(this.x * by, this.y * by)
+  }
+
+  add(p: PointLike) {
+    return new Point(this.x + p.x, this.y + p.y)
+  }
+
+  sub(p: PointLike) {
+    return new Point(this.x - p.x, this.y - p.y)
+  }
+
+  normalize() {
+    const mag = this.mag()
+    return new Point(this.x / mag, this.y / mag)
+  }
+
+  equals(p: PointLike) {
+    return this.x === p.x && this.y === p.y
+  }
+
+  copy() {
+    return new Point(this.x, this.y)
+  }
+
+  apply(func: (arg: number) => number) {
+    return new Point(func(this.x), func(this.y))
+  }
+
+  clamp(min: PointLike, max: PointLike) {
+    const res = this.copy()
+
+    if (this.x < min.x) {
+      res.x = min.x
+    } else if (this.x > max.x) {
+      res.x = max.x
     }
-  },
+
+    if (this.y < min.y) {
+      res.y = min.y
+    } else if (this.y > max.y) {
+      this.y = max.y
+    }
+
+    return res
+  }
+
+  static lerp(from: Point, to: Point, t: number) {
+    if (t <= 0) {
+      return from.copy()
+    } else if (t >= 1) {
+      return to.copy()
+    } else {
+      return from.add(to.sub(from).scale(t))
+    }
+  }
+
+  toString() {
+    return `x:${this.x}|y:${this.y}`
+  }
 }
