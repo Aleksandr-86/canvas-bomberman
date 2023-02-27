5703a97af86392f1912747b45de4d09cc9923ed7
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-02-27 16:17:41 +0300

    refactor(client): adjust the closing tag

--- packages/client/src/components/activityIndicator/activityIndicator.tsx
+++ packages/client/src/components/activityIndicator/activityIndicator.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './activityIndicator.module.css'
 
 export const ActivityIndicator: React.FC = () => (
-  <div className={styles.activityIndicator}></div>
+  <div className={styles.activityIndicator} />
 )
