4ce272182666f5991dc69155b04e7869c195b754
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-02-27 16:09:29 +0300

    refactor(client): simplify selectors

--- packages/client/src/store/selectors.ts
+++ packages/client/src/store/selectors.ts
@@ -1,27 +1,24 @@
 import { RootState } from './index'
+import { LEADERBOARD_LENGTH } from '../features/constants'
 
 export const getUser = (state: RootState) => state.user
 
 export const getLeaderboardState = (state: RootState) => {
   return state.leaderboard.stats.slice(
-    state.leaderboard.page * 5,
-    state.leaderboard.page * 5 + 5
+    state.leaderboard.page * LEADERBOARD_LENGTH,
+    state.leaderboard.page * LEADERBOARD_LENGTH + LEADERBOARD_LENGTH
   )
 }
 
-export const isFirstPage = (state: RootState) => {
-  return state.leaderboard.page === 0 ? true : false
-}
-
+export const isFirstPage = (state: RootState) => state.leaderboard.page === 0
 export const isLastPage = (state: RootState) => {
   const len = state.leaderboard.stats.length
   const range = (state.leaderboard.page + 1) * 5
   return range < len ? false : true
 }
 
-export const isLeaderboardLoading = (state: RootState) => {
-  return state.leaderboard.isLoading
-}
+export const isLeaderboardLoading = (state: RootState) =>
+  state.leaderboard.isLoading
 
 export const getCurrentScore = (state: RootState) => state.game.currentScore
 export const getGameStatus = (state: RootState) => state.game.status
