0519bd7f130760dd54e25454b9e6b6e24164e707
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-02-27 14:22:53 +0300

    refactor(client): simplify the return value

--- packages/client/src/components/activityIndicator/activityIndicator.tsx
+++ packages/client/src/components/activityIndicator/activityIndicator.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './activityIndicator.module.css'
 
-export const ActivityIndicator: React.FC = () => {
-  return <div className={styles.activityIndicator}></div>
-}
+export const ActivityIndicator: React.FC = () => (
+  <div className={styles.activityIndicator}></div>
+)
