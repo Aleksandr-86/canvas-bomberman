d7ebd83f5b98e4cb2b938f7c551517aa6113a6b3
German <bz5h7@yandex.ru>
2023-03-01 19:03:24 +0300

    fix(client): user chance util in createSoftWalls

--- packages/client/src/features/game/createSoftWalls.ts
+++ packages/client/src/features/game/createSoftWalls.ts
@@ -1,6 +1,6 @@
 import { GRID_WIDTH, WALL_GENERATION_CHANCE } from './const'
 import { type FieldState } from './types'
-import { type PointLike } from './utils/point'
+import { type PointLike, withChance } from './utils'
 import { Kind } from './types'
 
 /**
@@ -10,14 +10,14 @@ import { Kind } from './types'
  * @returns
  */
 
-export function createSoftWalls(field: FieldState, offset: PointLike) {
+export function generateWallPositions(field: FieldState, offset: PointLike) {
   const walls = []
 
   for (let i = 0; i < field.length; ++i) {
     const cell = field[i]
 
     if (cell === Kind.Empty) {
-      if (Math.random() * 101 < WALL_GENERATION_CHANCE) {
+      if (withChance(WALL_GENERATION_CHANCE)) {
         const x = i % GRID_WIDTH
         const y = Math.trunc(i / GRID_WIDTH)
 
