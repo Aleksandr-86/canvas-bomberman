070ccb018ef46ae63d354e22db1079c0bfd61531
archebaldo <zakharov90@gmail.com>
2023-03-09 22:54:19 +0300

    feat(client): update constants - add API_URL_CUSTOM

--- packages/client/src/features/constants.ts
+++ packages/client/src/features/constants.ts
@@ -12,3 +12,6 @@ export const TEAM_NAME = 'alt+f4v2'
  * на одной странице
  *  */
 export const LEADERBOARD_LENGTH = 5
+
+// Локальный сервер, будет заменен url Яндекс Практикума, после деплоя в облако
+export const API_URL_CUSTOM = `http://localhost:3001/api`
