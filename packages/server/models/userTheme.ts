bf49a28f9cdebbc85980d6c463ecd3a57af7b47d
archebaldo <zakharov90@gmail.com>
2023-03-09 22:47:13 +0300

    feat(server): add models for themes

---
+++ packages/server/models/userTheme.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Model,
+  Table,
+  PrimaryKey,
+  Column,
+  DataType,
+  AllowNull,
+} from 'sequelize-typescript'
+
+@Table({
+  timestamps: false,
+  paranoid: true,
+  tableName: 'user_theme',
+})
+export class UserTheme extends Model<UserTheme> {
+  @PrimaryKey
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  user_id!: number
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  value!: string
+}
