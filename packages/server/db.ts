59fc7b07052e80149d3477f353df52dbc8d7e994
archebaldo <zakharov90@gmail.com>
2023-03-09 22:50:03 +0300

    feat(server): add sequelize and configure db

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -1,28 +1,22 @@
-import { Client } from 'pg'
+const {
+  POSTGRES_HOST,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_PORT,
+} = process.env
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
-  process.env
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
+import { UserTheme } from './models/userTheme'
 
-export const createClientAndConnect = async (): Promise<Client | null> => {
-  try {
-    const client = new Client({
-      user: POSTGRES_USER,
-      host: 'localhost',
-      database: POSTGRES_DB,
-      password: POSTGRES_PASSWORD,
-      port: Number(POSTGRES_PORT),
-    })
-
-    await client.connect()
-
-    const res = await client.query('SELECT NOW()')
-    console.log('  ➜ 🎸 Connected to the database at:', res?.rows?.[0].now)
-    client.end()
-
-    return client
-  } catch (e) {
-    console.error(e)
-  }
-
-  return null
+const sequelizeOptions: SequelizeOptions = {
+  host: POSTGRES_HOST,
+  port: Number(POSTGRES_PORT),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
+  dialect: 'postgres',
 }
+
+export const sequelize = new Sequelize(sequelizeOptions)
+sequelize.addModels([UserTheme])
