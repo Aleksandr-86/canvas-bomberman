2f3c544a8983923189f6f940baccb23b65e7935b
archebaldo <zakharov90@gmail.com>
2023-03-12 16:40:37 +0300

    feat(client): update index.tsx (add feature for request auth and theme)

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -3,12 +3,45 @@ import { Routes, Route } from 'react-router-dom'
 import { routes } from '../routes'
 import { useAppDispatch } from '../store/hooks'
 import { me } from '../store/userActions'
+import { getCurrentTheme } from '../store/themeActions'
+import { oauth } from '../store/userActions'
+import { useSearchParams } from 'react-router-dom'
+import { REDIRECT_URI } from '../features/oauth/onOauthRequest'
+import { setTheme, Theme } from '../store/themeSlice'
 
 const AppRouting = () => {
   const dispatch = useAppDispatch()
+  const [searchParams] = useSearchParams()
+  const code = searchParams.get(`code`)
 
   useEffect(() => {
+    if (code) {
+      dispatch(oauth({ code, redirect_uri: REDIRECT_URI }))
+        .then(() => dispatch(me()).unwrap())
+        .then(async ({ id }) => {
+          dispatch(getCurrentTheme(id))
+        })
+      return
+    }
+
     dispatch(me())
+      .unwrap()
+      .then(async ({ id }) => {
+        dispatch(getCurrentTheme(id))
+      })
+      .catch(async error => {
+        if (error.status === 401) {
+          let theme = localStorage.getItem(`theme`)
+
+          if (!theme) {
+            theme = Theme.LIGHT
+            localStorage.setItem(`theme`, Theme.LIGHT)
+          }
+
+          document.documentElement.dataset.theme = theme
+          dispatch(setTheme(theme as Theme))
+        }
+      })
   }, [])
 
   return (
