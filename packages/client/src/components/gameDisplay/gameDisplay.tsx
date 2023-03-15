ebc2f3c34e4029d4e593d6d6437ab6d3e7f20ec1
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-03-15 06:51:44 +0300

    refactor(client): remove current stats from game start display

--- packages/client/src/components/gameDisplay/gameDisplay.tsx
+++ packages/client/src/components/gameDisplay/gameDisplay.tsx
@@ -4,17 +4,20 @@ import { GameOverlay } from '../gameOverlay/gameOverlay'
 import { useFullScreen } from '../../hooks/useFullScreen'
 import { GameScore } from '../gameScore'
 import styles from './gameDisplay.module.css'
+import { useAppSelector } from '../../store/hooks'
+import { getGameStatus } from '../../store/selectors'
+import { GameStatus } from '../../store/gameSlice'
 
 export const GameDisplay: React.FC = () => {
   const canvasWrapperRef = useRef<null | HTMLDivElement>(null)
   useFullScreen(canvasWrapperRef)
-
+  const status = useAppSelector(getGameStatus)
   const { canvasRef, startGame } = useGame()
 
   return (
     <div className={styles.gameDisplay}>
       <div className={styles.gameDisplayCanvasWrapper} ref={canvasWrapperRef}>
-        <GameScore />
+        {status === GameStatus.IN_PROGRESS && <GameScore />}
         <canvas ref={canvasRef} width={1280} height={640} />
         <GameOverlay startGame={startGame} />
       </div>
