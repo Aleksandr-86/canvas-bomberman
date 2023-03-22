0f8389e4794b169c9cfc61f4ec418c4aeb569523
German <bz5h7@yandex.ru>
2023-03-22 06:57:02 +0300

    refactor(server): authMiddleware redirect flow

--- packages/server/middlewares/authMiddleware.ts
+++ packages/server/middlewares/authMiddleware.ts
@@ -2,36 +2,29 @@ import type { RequestHandler } from 'http-proxy-middleware'
 import axios from 'axios'
 import { API_URL } from '../constants'
 
-const authRoutes = ['/profile', '/game', '/leaderboard']
-const signIn = '/sign-in'
+const protectedRoutes = ['/profile', '/game', '/leaderboard']
+const redirectRoute = '/sign-in'
 
 export const authMiddleware: RequestHandler = async (req, res, next) => {
-  const { cookies } = req.cookies
-
-  if (cookies && 'authCookie' in cookies) {
+  if (!req.headers.cookie) {
+    if (protectedRoutes.includes(req.originalUrl)) {
+      res.redirect(redirectRoute)
+    }
+  } else {
     try {
       const response = await axios(API_URL + '/auth/user', {
         headers: { Cookie: req.headers.cookie },
+        withCredentials: true,
       })
 
       if (response?.data?.id) {
         res.locals.user = response.data
       }
-
-      next()
     } catch (err) {
       res.clearCookie('authCookie')
       res.clearCookie('uuid')
 
-      if (authRoutes.includes(req.originalUrl)) {
-        res.redirect(signIn)
-      }
-
       next()
     }
-  } else if (authRoutes.includes(req.originalUrl)) {
-    res.redirect(signIn)
-  } else {
-    next()
   }
 }
