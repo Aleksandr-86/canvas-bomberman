2e0bca0af4bbf25ab33978272ee33f89aaee2b38
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-03-22 10:57:26 +0300

    refactor(client): move the variable outside the component

--- packages/client/src/components/gameScore/gameScore.tsx
+++ packages/client/src/components/gameScore/gameScore.tsx
@@ -3,11 +3,12 @@ import styles from './gameScore.module.css'
 import { selectGameStats } from '../../store/selectors'
 import { GAME_DURATION } from '../../features/game/const'
 
+const cssVariables = {
+  '--anim-duration': `${GAME_DURATION}s`,
+} as React.CSSProperties
+
 export const GameScore: React.FC = () => {
   const { score, inProgress } = useAppSelector(selectGameStats)
-  const cssVariables = {
-    '--anim-duration': `${GAME_DURATION}s`,
-  } as React.CSSProperties
 
   return (
     <div className={styles.panel}>
